9b0d6151b30ded7ef9ce5eccbd4a8d99503cb100
sspabhishek <abhishekprajapati243@gmail.com>
2024-03-15 19:20:37 +0530

    day 4

---
+++ 04_Operation.js
@@ -0,0 +1,45 @@
+// *********************************** Operations ********************************** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+// let str1 = "Hello "
+// let str2 = "Abhishek"
+
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log("Abhi\nKumar");
+
+// console.log(3 + 4 * 5 % 3);
+
+
+// console.log(true);  //true
+// console.log(+true); //1
+// console.log(true+); //error
+
+// console.log(+""); //0
+// console.log(typeof +true); 
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+
+++gameCounter;
+console.log(gameCounter);
