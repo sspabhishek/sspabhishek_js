8f3f4ef73869b0dcfbcb06c9804c55f25f40254e
sspabhishek <abhishekprajapati243@gmail.com>
2024-04-16 22:36:31 +0530

    compairson operator

---
+++ 05_Compairson.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+/*
+Note : The reason is that an equality check == and comparisons (>, <, >=, <= ) work differently.
+Comparision convert null to a number, treating it as 0. That's why 
+null >=0 is true and null > 0 is false.
+*/
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log(undefined);
+
+// Strict Check
