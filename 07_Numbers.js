2a89806ece8fea67a82b949852e8a96c56f44fb2
sspabhishek <abhishekprajapati243@gmail.com>
2024-06-25 08:05:59 +0530

    Done Numbers

---
+++ 07_Numbers.js
@@ -0,0 +1,35 @@
+const score = 400;
+// console.log(score); 
+
+const balance = new Number(400);
+// console.log(balance);
+
+console.log(typeof(balance.toString()).length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.4324
+// console.log(otherNumber.toPrecision(3)); //gives pricise value
+
+const hundreds =1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.pow(9,2));
+// console.log(Math.min(3,4,2,3,4,12,3,1));
+// console.log(Math.max(3,4,2,3,4,12,3,1));
+
+
+// console.log((Math.random()*10)+1);
+// console.log(Math.round((Math.random()*1000000)+1));
+// console.log(Math.floor((Math.random()*10)+1));
+// console.log(Math.ceil((Math.random()*100)+1));
+
+const MIN = 10;
+const MAX = 20;
+
+console.log(Math.floor(Math.random()* (MAX-MIN+1)+MIN));
